c326342fbd1c1a0e66b72a47ba6e2b8a1974d067
Andrew Forward <aforward@gmail.com>
2017-12-03 17:44:18 -0500

    Refactor deposit into the success and failure implementations

--- lib/bank.ex
+++ lib/bank.ex
@@ -1,7 +1,11 @@
 defmodule Bank do
 
-  def deposit(account, deposit_amount) do
+  def deposit(account, deposit_amount) when deposit_amount >= 0 do
     {:ok, account + deposit_amount}
+  end
+
+  def deposit(_account, _invalid_amount) do
+    {:error, "Cannot deposit a negative amount."}
   end
 
 end
