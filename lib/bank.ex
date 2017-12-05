1f955051f97e43a8b70af8a09824ebb3f995aaf9
Andrew Forward <aforward@gmail.com>
2017-12-05 09:10:49 -0500

    Move from unit tests to doctests

--- lib/bank.ex
+++ lib/bank.ex
@@ -1,5 +1,21 @@
 defmodule Bank do
 
+  @doc """
+  Deposit money into your bank account, and return the current
+  balance in your account.
+
+  ## Examples
+
+      iex> Bank.deposit(0, 10)
+      {:ok, 10}
+
+      iex> Bank.deposit(10, 5)
+      {:ok, 15}
+
+      iex> Bank.deposit(10, -5)
+      {:error, "Cannot deposit a negative amount."}
+
+  """
   def deposit(account, deposit_amount) when deposit_amount >= 0 do
     {:ok, account + deposit_amount}
   end
--- test/bank_test.exs
+++ test/bank_test.exs
@@ -1,13 +1,4 @@
 defmodule BankTest do
   use ExUnit.Case
-
-  test "deposit into acccount" do
-    assert {:ok, 10} == Bank.deposit(0, 10)
-    assert {:ok, 15} == Bank.deposit(10, 5)
-  end
-
-  test "cannot deposit a negative amount" do
-    assert {:error, "Cannot deposit a negative amount."} == Bank.deposit(10, -5)
-  end
-
+  doctest Bank
 end
